b37097aa790cd83b9e8cf6a70a3c2fdb6df2f783
mirmalis <10066881+mirmalis@users.noreply.github.com>
2023-12-01 21:34:51 +0200

    Update test.js

--- other/test.js
+++ other/test.js
@@ -1,31 +1,3 @@
 function SDPAltert(){
   setInterval(() => alert(123), 5000);
 }
-function SDPInject(){
-  function InjectJsonHook(modify, handle){
-    Response.prototype.MyJson = Response.prototype.json;
-    Response.prototype.json = async function () {
-      let json = await this.MyJson();
-      let modifiedJson = modify(this, json);
-      handle(this, modifiedJson);
-      return modifiedJson;
-    };
-  }
-  alert('SDPInjecting');
-  InjectJsonHook(
-    async (Response, Data) => Data,
-    async (Response, Data) => {
-      if (!Data) {
-        return;
-      }
-      
-      let asd = {
-        "Data": Data,
-        "To": Response.url,
-        "From": document.location.href,
-        Response
-      };
-      console.log(asd);
-    }
-  );
-}
